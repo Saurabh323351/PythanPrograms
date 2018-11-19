6f28fd96b121ff16a6b51d20e9a62c78c75652dc
Saurabh323351 <44753622+Saurabh323351@users.noreply.github.com>
2018-11-20 00:36:41 +0530

    Triplet functional part committed

---
+++ Triplet.py
@@ -0,0 +1,11 @@
+from com.bridgelab.utility.Utility import Utility
+utility_obj=Utility()
+print("Enter Number of values you want to enter")
+no_of_values=utility_obj.get_int()
+print("Enter values now")
+store_values=[]
+for i in range(0,no_of_values):
+    value=utility_obj.get_int()
+    store_values.append(value)
+
+utility_obj.find_triplet(store_values)
