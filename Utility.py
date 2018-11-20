83253606c5bb4911d6fa352bee86297c979df3e2
Saurabh323351 <44753622+Saurabh323351@users.noreply.github.com>
2018-11-20 19:27:32 +0530

    Add files via upload

--- Utility.py
+++ Utility.py
@@ -1,6 +1,9 @@
 import random
 import sys
 from array import array
+from math import sqrt,floor,ceil
+import time
+from timeit import default_timer
 
 class Utility:
 
@@ -46,18 +49,20 @@ class Utility:
                 print(str(year) + " is not a leap year")
 
     def power(self, limit):
-        #store_value=[]
+        store_value=[]
         #count=0
         if 0 > limit or limit >30:
             print("Enter value greater than equal to zero but less than 31")
             limit=utility_obj.get_int()
-            utility_obj.power(limit)
+            if 0 > limit or limit > 30:
+                return utility_obj.power(limit)
 
-        else:
+        if  0<=limit<=30:
 
             for i in range(0,limit+1):
                 value = pow(2,i)
-                print(value)
+                store_value.append(value)
+            return store_value
 
     def get_nth_harmonic_value(self,harmonic_value):
         sum=0
@@ -111,48 +116,184 @@ class Utility:
 
         win_percent = (win_count / (win_count + lose_count)) * 100
         lose_percent = (lose_count / (win_count + lose_count)) * 100
-        return win_count,win_percent,lose_percent
+        return win_count, win_percent, lose_percent
 
 
-    def coupan_generator(self,no_of_coupans):
-        store_coupans=[]
-        random_number_count=0
-        while len(store_coupans)<= no_of_coupans:
-            random_number=random.randint(1,50)
-            random_number_count+=1
+    def coupan_generator(self, no_of_coupans):
+            store_coupans = []
+            random_number_count = 0
+            while len(store_coupans) < no_of_coupans:
+                random_number = random.randint(1, 50)
+                random_number_count += 1
 
-            if store_coupans.__contains__(random_number)==False:
-                store_coupans.append(random_number)
+                if store_coupans.__contains__(random_number) == False:
+                    store_coupans.append(random_number)
 
-
-        return random_number_count,store_coupans
-
-
-    def get_2d__input(self,row,column):
-
-        two_d_array = [[0 for j in range(column)] for i in range(row)]
-
-        for i in range(0,row):
-            for j in range(0,column):
-                two_d_array[i][j]=Utility().get_int()
-
-        return two_d_array
+            return random_number_count, store_coupans
 
 
-    def find_triplet(self,store_values):
-        size=len(store_values)
+    def get_2d__input(self, row, column):
 
-        for i in range(0,size-2):
+            two_d_array = [[0 for j in range(column)] for i in range(row)]
 
-            for j in range(i+1,size-1):
+            for i in range(0, row):
+                for j in range(0, column):
+                    two_d_array[i][j] = Utility().get_int()
 
-                for k in range(j+1,size):
+            return two_d_array
 
-                    if (store_values[i]+store_values[j]+store_values[k]) == 0:
+    def find_triplet(self, store_values):
+            size = len(store_values)
 
-                        print(store_values[i],end=" ")
-                        print(store_values[j],end=" ")
-                        print(store_values[k],end=" ")
-                        print("")
+            for i in range(0, size - 2):
+
+                for j in range(i + 1, size - 1):
+
+                    for k in range(j + 1, size):
+
+                        if (store_values[i] + store_values[j] + store_values[k]) == 0:
+                            print(store_values[i], end=" ")
+                            print(store_values[j], end=" ")
+                            print(store_values[k], end=" ")
+                            print("")
+
+
+    def get_euclidean_distance(self,x_coordinate,y_coordinate):
+
+        x=x_coordinate
+        y=y_coordinate
+
+        euclidean_distance=sqrt(x**2+y**2)
+        return euclidean_distance
+
+
+    def get_elapsed_time(self,choice):
+
+        if choice==1:
+            print("StopWatch Started")
+            start_time=default_timer()
+
+        choice=Utility().get_int()
+        if choice==2:
+            stop_time=default_timer()
+
+            elapsed_time_in_seconds=stop_time-start_time
+            print(elapsed_time_in_seconds)
+
+
+    def get_quadratic_roots(self,a,b,c):
+
+        delta=(b*b-4*a*c)
+
+        first_root=(-b+sqrt(delta))/2*a
+
+        second_root=(-b-sqrt(delta))/2*a
+
+        return first_root,second_root
+
+
+    def get_effective_temperature(self,temperature,velocity):
+        t=temperature
+        v=velocity
+
+        if t>=51 or (v<=2 or v>=121):
+            print("Enter temperature value less than equal to 50 \n and velocity value greater than 2 but less than equal to 120")
+            t=Utility().get_int()
+            v = Utility().get_int()
+            if t >= 51 or (v <= 2 or v >= 121):
+              return  Utility().get_effective_temperature(t,v)
+
+
+        if t<=50 and v<=120:
+            windchill = 35.74 + 0.6215 * t + (.4275 * t - 35.75) * pow(v, 0.16)
+            return windchill
+
+
+    def anagram_detection(self,first_string,second_string):
+        first_string=first_string
+        s2=second_string
+        first_string_list=[]
+        s2_list=[]
+
+        lower_string=first_string.lower()
+        first_string=lower_string
+
+        lower_string=''
+        lower_string=s2.lower()
+        s2=lower_string
+
+        remove_space=''
+        for i in range(0,len(first_string)):
+            if first_string[i]==' ':
+                continue
+
+            remove_space+=first_string[i]
+
+        first_string=remove_space
+
+        remove_space = ''
+        for i in range(0, len(s2)):
+            if s2[i] == ' ':
+                continue
+            remove_space += s2[i]
+
+        s2 = remove_space
+
+        for i in first_string:
+            first_string_list.append(i)
+
+
+        for i in range(0,len(first_string_list)-1):
+
+            for j in range(i+1,len(first_string_list)):
+                temp=''
+                if first_string_list[i]>first_string_list[j]:
+                    temp=first_string_list[i]
+                    first_string_list[i]=first_string_list[j]
+                    first_string_list[j]=temp
+
+        first_string=''
+        for i in range(0, len(first_string_list)):
+            first_string=first_string+first_string_list[i]
+
+
+
+        for i in s2:
+            s2_list.append(i)
+
+        for i in range(0,len(s2_list)-1):
+
+            for j in range(i + 1,len(s2_list)):
+                temp = ''
+                if s2_list[i] > s2_list[j]:
+                    temp = s2_list[i]
+                    s2_list[i] = s2_list[j]
+                    s2_list[j] = temp
+
+        s2 = ''
+        for i in range(0, len(s2_list)):
+            s2 = s2 + s2_list[i]
+
+        s3=''
+        if first_string==s2:
+            print("Both Strings are Anagram of each other")
+
+
+        else:
+            print('Both Strings are not Anagram of each other')
+
+    def get_prime(self):
+        list = []
+        is_prime = True
+        for i in range(1001):
+            if i == 0 or i == 1:
+                continue
+            is_prime = True
+            for j in range(2, i):
+                if i % j == 0:
+                    is_prime = False
+            if is_prime:
+                list.append(i)
+        return list
 
 utility_obj = Utility()
