80a1dc8968a84bec60fa1ea60e05307aefd4e64a
Saurabh323351 <44753622+Saurabh323351@users.noreply.github.com>
2018-11-28 19:32:30 +0530

    Add files via upload

--- datastructure_util.py
+++ datastructure_util.py
@@ -34,6 +34,11 @@ class LinkedList:
     def show(self):
 
         traverse = self.head
+
+        if self.head == None:
+            print("Linked List  is empty")
+            return
+
         while traverse.next != None:
             print(traverse.data)
             traverse = traverse.next
@@ -97,15 +102,23 @@ class LinkedList:
 
         traverse = self.head
 
+        if self.head == None:
+            return -1
+
+        if self.head.next == None:
+            self.head = None
+            return traverse.data
+
         while traverse.next is not None:
 
             t1 = traverse.next
+
             if t1.next is None:
                 traverse.next = None
                 return t1.data
             traverse = traverse.next
 
-    def pop(self, position):
+    def pop_position(self, position):
 
         traverse = self.head
         temp = self.head
@@ -141,15 +154,21 @@ class LinkedList:
 
 
 # if __name__=='__main__ ':
+#
+#
+# linkedlist_obj = LinkedList()
+# linkedlist_obj.append(10)
+# linkedlist_obj.append(20)
+# linkedlist_obj.append(30)
+# linkedlist_obj.append(40)
+# linkedlist_obj.append(50)
+# print(linkedlist_obj.size())
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
 
-
-linkedlist_obj = LinkedList()
-# list.append(10)
-# list.append(20)
-# list.append(30)
-# list.append(40)
-# list.append(50)
-# list.append(60)
 # list.append("rajat")
 # list.insert(2,80)
 # list.insert(7,90)
@@ -157,41 +176,44 @@ linkedlist_obj = LinkedList()
 # print(list.search_item("saurabh"))
 # print(list.size())
 # print(list.index(90))
-
-
-# list.pop(3)
 #
-# list.show()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# linkedlist_obj.pop()
+# print(linkedlist_obj.pop())
+# linkedlist_obj.show()
+
 # list.pop()
 # list.show()
 
-file=open("LinkedList_File","w+")
-list=['saurabh ','rajat ','shubham ','rohini']
-file.writelines(list)
-file.close()
-file=open("LinkedList_File","r")
-list=file.readlines()
-s=list[0]
-list=s.split()
-#print(list)
-
-for i in list:
-    linkedlist_obj.append(i)
-
+# file=open("LinkedList_File","w+")
+# list=['saurabh ','rajat ','shubham ','rohini']
+# file.writelines(list)
+# file.close()
+# file=open("LinkedList_File","r")
+# list=file.readlines()
+# s=list[0]
+# list=s.split()
+# #print(list)
+#
+# for i in list:
+#     linkedlist_obj.append(i)
+#
+# # linkedlist_obj.show()
+#
+# result=linkedlist_obj.search_item('ram')
+#
+# if result==True:
+#     linkedlist_obj.remove('ram')
+#
+# if result==False:
+#     linkedlist_obj.append('ram')
+#
+#
 # linkedlist_obj.show()
-
-result=linkedlist_obj.search_item('shubh')
-
-if result==True:
-    linkedlist_obj.remove('shubh')
-
-if result==False:
-    linkedlist_obj.append('shubh')
-
-
-linkedlist_obj.show()
-
-
 
 
 class OrderedList:
@@ -213,19 +235,106 @@ class OrderedList:
                 node.next = traverse
 
             if self.head.data < node.data:
-               temp = self.head
-               while traverse.next!=None:
-                   if traverse.data<node.data:
-                       temp=traverse
-                   traverse = traverse.next
+                temp = self.head
+                while traverse.next != None:
+                    if traverse.data < node.data:
+                        temp = traverse
+                    traverse = traverse.next
 
-               if traverse.data < node.data:
-                   temp = traverse
+                if traverse.data < node.data:
+                    temp = traverse
 
-               temp1 = temp.next
-               temp.next= node
-               node.next = temp1
+                temp1 = temp.next
+                temp.next = node
+                node.next = temp1
 
+    def remove(self, data):
+
+        traverse = self.head
+        temp = self.head
+        if traverse.data == data:
+            self.head = traverse.next
+            return traverse.data
+
+        while traverse.next != None:
+
+            temp = traverse.next
+            if temp.data == data:
+                traverse.next = temp.next
+                return temp.data
+
+            traverse = traverse.next
+
+    def search_item(self, data):
+
+        traverse = self.head
+        while traverse.next != None:
+
+            if traverse.data == data:
+                return True
+            traverse = traverse.next
+        if traverse.data == data:
+            return True
+        else:
+            return False
+
+    def is_empty(self):
+
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def size(self):
+        traverse = self.head
+        count = 1
+        while traverse.next != None:
+            traverse = traverse.next
+            count += 1
+        return count
+
+    def index(self, data):
+
+        traverse = self.head
+        count = 0
+        while traverse.next != None:
+
+            if traverse.data == data:
+                return count
+            traverse = traverse.next
+            count += 1
+
+        if traverse.data == data:
+            return count
+
+    def pop(self):
+
+        traverse = self.head
+
+        while traverse.next is not None:
+
+            t1 = traverse.next
+            if t1.next is None:
+                traverse.next = None
+                return t1.data
+            traverse = traverse.next
+
+    def pop_position(self, position):
+
+        traverse = self.head
+        temp = self.head
+
+        if position == 0:
+            self.head = traverse.next
+            return traverse.data
+
+        for i in range(0, position - 1):
+            traverse = traverse.next
+
+        temp = traverse
+        traverse = traverse.next
+        temp.next = traverse.next
+        return traverse.data
 
     def show(self):
         traverse = self.head
@@ -236,14 +345,229 @@ class OrderedList:
 
 
 ordered_list = OrderedList()
-ordered_list.add(10)
-ordered_list.add(1)
-ordered_list.add(2)
-ordered_list.add(3)
-ordered_list.add(4)
-ordered_list.add(11)
-ordered_list.add(5)
-ordered_list.add(-1)
-ordered_list.add(-2)
-ordered_list.add(6)
-ordered_list.show()
+
+
+# ordered_list.add(10)
+# ordered_list.add(1)
+# ordered_list.add(2)
+# ordered_list.add(3)
+# ordered_list.add(4)
+# ordered_list.add(-11)
+# ordered_list.add(5)
+# ordered_list.add(-1)
+# ordered_list.add(-2)
+# ordered_list.add(6)
+
+# ordered_list.remove(-11)
+# ordered_list.remove(10)
+# print(ordered_list.search_item(0))
+# print(ordered_list.is_empty())
+# print(ordered_list.index(1))
+# print(ordered_list.pop())
+# print(ordered_list.pop_position(0))
+# print(ordered_list.size())
+# ordered_list.show()
+
+
+class Stack:
+    top = 0
+    head = None
+
+    def __init__(self):
+        pass
+
+    def push(self, data):
+
+        node = Node(data)
+
+        if self.head == None:
+
+            self.head = node
+
+
+        else:
+
+            traverse = self.head
+
+            while traverse.next != None:
+                traverse = traverse.next
+
+            traverse.next = node
+
+    def size(self):
+        traverse = self.head
+
+        if self.head == None:
+            return 0
+        size = 1
+        while traverse.next != None:
+            traverse = traverse.next
+            size += 1
+        return size
+
+    def show(self):
+        traverse = self.head
+
+        if self.top <= -1:
+            print(" Stack Underflow")
+            return
+        if traverse == None:
+            print("Stack is empty")
+            return
+
+        while traverse.next != None:
+            print(traverse.data)
+            traverse = traverse.next
+        print(traverse.data)
+
+    def pop(self):
+
+        traverse = self.head
+
+        if self.head == None:
+            return -1
+
+        if self.head.next == None:
+            self.head = None
+
+            return traverse.data
+
+        while traverse.next is not None:
+
+            t1 = traverse.next
+            if t1.next is None:
+                traverse.next = None
+
+                return t1.data
+            traverse = traverse.next
+
+    def peek(self):
+        traverse = self.head
+
+        if self.head == None:
+            return "empty stack"
+        self.top = self.size() - 1
+        for i in range(0, self.top):
+            traverse = traverse.next
+
+        return traverse.data
+
+    def is_empty(self):
+
+        if self.size() == 0:
+            return True
+        else:
+            return False
+
+
+stack = Stack()
+
+# stack.push(56)
+# stack.push(57)
+# stack.push(58)
+# stack.push(59)
+# stack.push(60)
+# stack.push(61)
+# stack.push(62)
+
+
+# stack.pop(57)
+# stack.pop(58)
+# stack.pop(59)
+# stack.pop(60)
+# stack.pop(61)
+# stack.pop(62)
+# stack.pop(63)
+# print(stack.size())
+# print(stack.peek())
+# print(stack.is_empty())
+# stack.show()
+
+string="{(([{}]))}"
+
+for i in string:
+    #print(i)
+    if i == '(' or i=='[' or i=='{':
+        stack.push(i)
+        # stack.show()
+
+
+
+    if ((stack.peek() == '(' and i==')') or (stack.peek() == '[' and i==']' )or (stack.peek() == '{' and i=='}') ) and stack.size()>0:
+        stack.pop()
+        continue
+
+
+
+    if i == ')'or  i==']' or i=='}':
+        stack.push(i)
+#
+# if stack.size()==0:
+#     print("Balanced Parenthesis ")
+# else:
+#     print("Parenthesis is not Balanced ")
+# print(stack.size())
+# stack.show()
+
+
+class Queue:
+
+    front=None
+    rear=None
+
+    def __init__(self):
+        pass
+
+
+    def enqueue(self, data):
+
+        node = Node(data)
+
+        if self.front == None and self.rear==None:
+
+            self.front = node
+            self.rear=node
+
+        else:
+
+
+
+            # while traverse.next != None:
+            #     traverse = traverse.next
+
+            self.rear.next = node
+            self.rear=self.rear.next
+
+    def show(self):
+
+
+
+        if self.front == None:
+            print("Linked List  is empty")
+            return
+
+        while self.front.next != None:
+            print(self.front.data)
+            self.front = self.front.next
+
+        print(self.front.data)
+
+    def dequeue(self):
+
+        temp=self.front
+        self.front=self.front.next
+        return temp.data
+
+queue=Queue()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.show()
