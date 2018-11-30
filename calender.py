2a9420266de93eea7eba5820a0fbe4bbe3ffb6e9
Saurabh Singh <44753622+Saurabh323351@users.noreply.github.com>
2018-11-30 19:03:32 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,12 @@
+from com.bridgelabz.util.datastructure_util import *
+from com.bridgelabz.util.utility import *
+
+utility_obj=Utility()
+
+logic_obj = Logic()
+
+print('Enter Month')
+month=utility_obj.get_int()
+print('Enter Year')
+year=utility_obj.get_int()
+logic_obj.calender(month,year)
