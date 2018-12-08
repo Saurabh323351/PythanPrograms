b792d5c2d425cc14711045aa20fc93d4e622941d
Saurabh Singh <44753622+Saurabh323351@users.noreply.github.com>
2018-12-08 18:53:11 +0530

    Add files via upload

---
+++ regular_expression.py
@@ -0,0 +1,11 @@
+from com.bridgelabz.util.object_oriented_util import RegularExpression
+
+
+def re_runner():
+    obj = RegularExpression()
+    print("The Modified String with user information are as follows")
+    print(obj.regex())
+
+
+if __name__ == "__main__":
+    re_runner()
